acf3124f1a57417b9433743c24801f88d32ab27b
DOCENCIA <DOCENCIA@U019320.xa.bp.ehu.es>
2015-11-24 08:17:07 +0100

    Actualizacion

--- Sudoku/src/packSudoku/GestorLogros.java
+++ Sudoku/src/packSudoku/GestorLogros.java
@@ -5,41 +5,16 @@ import java.util.Iterator;
 
 public class GestorLogros {
 	
-	ArrayList<Logros> miListaLogros=new ArrayList<Logros>();
+	ListaLogros miListaLogros;
 
 	
 	private GestorLogros(){
 		if(miListaLogros==null){
-			miListaLogros=new ArrayList<Logros>();
+			miListaLogros=new ListaLogros();
 		}
 	}
 	
-	public void anadirLogro(Logros logro){
-		this.miListaLogros.add(logro);
-	}
 	
-	private Iterator<Logros> getIterador() {
-		return this.miListaLogros.iterator() ;
-	}
-
-	public Logros buscarLogro(String codLogro){
-		Iterator<Logros> it= getIterador();
-		Logros logro = null;
-		boolean flag=false;
-		while(it.hasNext()&&flag!=true){
-			logro= it.next();
-			if(logro.codLogro==codLogro){
-				flag=true;
-			}
-			
-		}
-		return logro;
-	}
-	
-	public void eliminarLogro(String codLogro){
-		this.miListaLogros.remove(buscarLogro(codLogro));	
-		}
-
 	
 	
 }
