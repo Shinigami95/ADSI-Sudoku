246a45723fa621c047352b9a554a5d99708a279a
Shinigami95 <Shinigami95@users.noreply.github.com>
2015-12-29 13:13:40 +0100

    Logros terminado

--- Sudoku/src/packModelo/Logros.java
+++ Sudoku/src/packModelo/Logros.java
@@ -1,7 +1,9 @@
 package packModelo;
 
+import javax.swing.JOptionPane;
+
 import packControladores.ConexionBD;
-import packExcepciones.ExcepcionConectarBD;
+import packSudoku.excepciones.ExcepcionConectarBD;
 
 public class Logros {
 	
@@ -15,21 +17,40 @@ public class Logros {
 		this.descripcionLogro=descripcion;
 	}
 	
-	public static void tipoLogro(String codL,String codS,String descripcion,int puntos,int numJug) throws ExcepcionConectarBD{
-		ConexionBD.getConexionBD().actualizarBD("INSERT INTO LOGRO(ID_L,ID_SUDOKU,DESCRIPCION) VALUES('"+codL+"','"+codS+"','"+descripcion+"')");
-		if(puntos>0){
-			if(numJug>0){
-				LogrosPuntuacionX.logrosPuntuacionX(codL, puntos);
+	public static void tipoLogro(String codL,String codS,String descripcion,String puntos,String numJug) throws ExcepcionConectarBD{
+		String log=codL;
+		if(Integer.parseInt(puntos)>0){
+			if(Integer.parseInt(numJug)<1){
+				log="X"+log;
+				ConexionBD.getConexionBD().actualizarBD("INSERT INTO LOGRO(ID_L,ID_SUDOKU,DESCRIPCION) VALUES('"+log+"','"+codS+"','"+descripcion+"');");
+				LogrosPuntuacionX.logrosPuntuacionX(log, puntos);
 			}
 			else{
-				LogrosPuntuacionXY.logrosPuntuacionXY(codL, puntos, numJug); 
+				log="Y"+log;
+				ConexionBD.getConexionBD().actualizarBD("INSERT INTO LOGRO(ID_L,ID_SUDOKU,DESCRIPCION) VALUES('"+log+"','"+codS+"','"+descripcion+"');");
+				LogrosPuntuacionXY.logrosPuntuacionXY(log, puntos, numJug); 
 			}
 		}
 		else{
-			LogrosResolucion.logrosResolucion(codL, numJug);
+			log="R"+log;
+			ConexionBD.getConexionBD().actualizarBD("INSERT INTO LOGRO(ID_L,ID_SUDOKU,DESCRIPCION) VALUES('"+log+"','"+codS+"','"+descripcion+"');");
+			LogrosResolucion.logrosResolucion(log, numJug);
 		}
 	}
-
+	
+	public static void modificarLogros(String codL,String codS,String descripcion,String puntos,String numJug) throws ExcepcionConectarBD{
+		ConexionBD.getConexionBD().actualizarBD("UPDATE LOGRO SET ID_SUDOKU='"+codS+"',DESCRIPCION='"+descripcion+"' WHERE ID_L='"+codL+"';");
+		if(codL.charAt(0)=='X'){
+			LogrosPuntuacionX.modificarLogro(codL, puntos);
+		}
+		else if (codL.charAt(0)=='Y') {
+			LogrosPuntuacionXY.modificarLogro(codL, puntos, numJug);
+		}
+		else if (codL.charAt(0)=='R') {
+			LogrosResolucion.modificarLogro(codL, numJug);
+		}
+		else{JOptionPane.showMessageDialog(null, "Datos incorrectos.");}
+	}
 
 
 }
