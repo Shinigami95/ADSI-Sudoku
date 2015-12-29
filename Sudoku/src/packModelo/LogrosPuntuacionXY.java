4745f31c6fd871454b7c3e2e7776dc8ed6cb4a45
Shinigami95 <Shinigami95@users.noreply.github.com>
2015-12-29 13:15:33 +0100

    logros_ptoxy terminado

--- Sudoku/src/packModelo/LogrosPuntuacionXY.java
+++ Sudoku/src/packModelo/LogrosPuntuacionXY.java
@@ -1,24 +1,28 @@
 package packModelo;
 
 import packControladores.ConexionBD;
-import packExcepciones.ExcepcionConectarBD;
+import packSudoku.excepciones.ExcepcionConectarBD;
 
 public class LogrosPuntuacionXY extends Logros {
 	
-	int puntuacion;
-	int jugadores;
+	String puntuacion;
+	String jugadores;
 	
-	public LogrosPuntuacionXY(String codL,String codS,String descripcion,int puntos,int jugadores)  {
+	public LogrosPuntuacionXY(String codL,String codS,String descripcion,String puntos,String jugadores)  {
 		super(codS,codL, descripcion);
 		this.puntuacion=puntos;
 		this.jugadores=jugadores;
 		
 	}
 	
-	public static void logrosPuntuacionXY(String codL,int puntos,int jugadores) throws ExcepcionConectarBD 
+	public static void logrosPuntuacionXY(String codL,String puntos,String jugadores) throws ExcepcionConectarBD 
 	{
-		ConexionBD.getConexionBD().actualizarBD("INSERT INTO LOGRO_PTOXY(ID_L,PTO,NUM_JUG) VALUES('"+codL+"','"+puntos+"','"+jugadores+"')");
+		ConexionBD.getConexionBD().actualizarBD("INSERT INTO LOGRO_PTOXY(ID_L,PTO,NUM_JUG) VALUES('"+codL+"','"+puntos+"','"+jugadores+"');");
 	}
+	public static void modificarLogro(String codL,String puntos,String jugadores) throws ExcepcionConectarBD
+	{
+		ConexionBD.getConexionBD().actualizarBD("UPDATE LOGRO_PTOXY SET NUM_JUG='"+jugadores+"', PTO='"+puntos+"' WHERE ID_L='"+codL+"';");
+		}
 }
 
 
