7e37c6ef311c44bd40d80a0720c10635d52b4251
Shinigami95 <Shinigami95@users.noreply.github.com>
2015-12-29 13:14:42 +0100

    Terminado logros_ptox

--- Sudoku/src/packModelo/LogrosPuntuacionX.java
+++ Sudoku/src/packModelo/LogrosPuntuacionX.java
@@ -1,18 +1,22 @@
 package packModelo;
 
 import packControladores.ConexionBD;
-import packExcepciones.ExcepcionConectarBD;
+import packSudoku.excepciones.ExcepcionConectarBD;
 
 public class LogrosPuntuacionX extends Logros{
 	
-	int puntuacion;
+	String puntuacion;
 	
-	public LogrosPuntuacionX(String codL,String codS,String descripcion,int puntos){
+	public LogrosPuntuacionX(String codL,String codS,String descripcion,String puntos){
 		super(codS,codL, descripcion);
 		this.puntuacion=puntos;
 		
 	}
-	public static void logrosPuntuacionX(String codL,int puntos) throws ExcepcionConectarBD{
-		ConexionBD.getConexionBD().actualizarBD("INSERT INTO LOGRO_PTOX(ID_L,PTO) VALUES('"+codL+"','"+puntos+"')");
+	public static void logrosPuntuacionX(String codL,String puntos) throws ExcepcionConectarBD{
+		ConexionBD.getConexionBD().actualizarBD("INSERT INTO LOGRO_PTOX(ID_L,PTO) VALUES('"+codL+"','"+puntos+"');");
 	}
+	public static void modificarLogro(String codL,String puntos) throws ExcepcionConectarBD
+	{
+		ConexionBD.getConexionBD().actualizarBD("UPDATE LOGRO_PTOX SET PTO='"+puntos+"' WHERE ID_L='"+codL+"';");
+		}
 }
