c2c7b53d36123482b9ec3ac2c7b12cf552f25ac4
Shinigami95 <Shinigami95@users.noreply.github.com>
2016-01-04 14:13:04 +0100

    Comentarios

--- Sudoku/src/packModelo/LogrosResolucion.java
+++ Sudoku/src/packModelo/LogrosResolucion.java
@@ -5,19 +5,12 @@ import packExcepciones.ExcepcionConectarBD;
 
 
 public class LogrosResolucion extends Logros{
-	
-	String jugadores;
-	
-	public LogrosResolucion(String codL,String codS,String descripcion,String jugador){
-		super(codS,codL, descripcion);
-		this.jugadores=jugador;
-		
-	}
+	//Metodo que mete el tipo de logro resolucion en la bd
 	public static void logrosResolucion(String codL,String jugador) throws ExcepcionConectarBD
 	{
 		ConexionBD.getConexionBD().actualizarBD("INSERT INTO LOGRO_RES(ID_L,NUM_JUG) VALUES('"+codL+"','"+jugador+"');");
 	}
-	
+	//Metodo que modifica los datos  del logro de tipo logro resolucion en la bd
 	public static void modificarLogro(String codL,String numJug) throws ExcepcionConectarBD
 	{
 		ConexionBD.getConexionBD().actualizarBD("UPDATE LOGRO_RES SET NUM_JUG='"+numJug+"' WHERE ID_L='"+codL+"';");
