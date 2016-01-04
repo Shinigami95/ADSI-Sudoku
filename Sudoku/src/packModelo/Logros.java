d8eb8ab485e368ab913cdf34174e853a0c5b6156
Shinigami95 <Shinigami95@users.noreply.github.com>
2016-01-04 14:11:06 +0100

    Comentarios

--- Sudoku/src/packModelo/Logros.java
+++ Sudoku/src/packModelo/Logros.java
@@ -8,40 +8,46 @@ import packExcepciones.ExcepcionConectarBD;
 
 public class Logros {
 	
-	String codSudoku;
-	String codLogro;
-	String descripcionLogro;
-	
-	public Logros(String codS,String codL,String descripcion){
-		this.codLogro=codL;
-		this.codSudoku=codS;
-		this.descripcionLogro=descripcion;
-	}
-	
+	//Este metodo mete en la bd los datos basicos del logro(id,descripcion y sudoku al que pertenece) y clasifica el logro segun sus caracteristicas.
 	public static void tipoLogro(String codL,String codS,String descripcion,String puntos,String numJug) throws ExcepcionConectarBD{
+		//Primero se crea una variable a la que le damos el valor del id del logro que se nos pasa.
 		String log=codL;
+		//Esta parte es la clasificacion del logro. Si la puntuacion que se nos pasa es mayor que 0 puede ser dos tipos de logro.
 		if(Integer.parseInt(puntos)>0){
+			//Si el numero de jugadores que la pueden conseguir es 0 este logro se consigue al llegar a una puntuacion x.
 			if(Integer.parseInt(numJug)<1){
+				//En tal caso hacemos que el ID del logro tenga un primer caracter X que me ayuda a clasificarlo en otros metodos.
 				log="X"+log;
+				//Ahora se añade a la bd en Logros y llamamos a un metodo que lo mete en la bd como un logro de tipo LogroPuntuacionX
 				ConexionBD.getConexionBD().actualizarBD("INSERT INTO LOGRO(ID_L,ID_SUDOKU,DESCRIPCION) VALUES('"+log+"','"+codS+"','"+descripcion+"');");
 				LogrosPuntuacionX.logrosPuntuacionX(log, puntos);
 			}
+			//Si el numero de jugadores que la pueden conseguir es mayor que 0 este logro se consigue al ser uno de los y primeros en llegar a una puntuacion x.
 			else{
+				//En tal caso hacemos que el ID del logro tenga un primer caracter Y que me ayuda a clasificarlo en otros metodos.
 				log="Y"+log;
+				//Ahora se añade a la bd en Logros y llamamos a un metodo que lo mete en la bd como un logro de tipo LogroPuntuacionXY
 				ConexionBD.getConexionBD().actualizarBD("INSERT INTO LOGRO(ID_L,ID_SUDOKU,DESCRIPCION) VALUES('"+log+"','"+codS+"','"+descripcion+"');");
 				LogrosPuntuacionXY.logrosPuntuacionXY(log, puntos, numJug); 
 			}
 		}
+		//Si la puntuacion que se nos de es 0 entoces es un logro que se consigue al ser uno de los x primeros en completar el sudoku.
 		else{
+			//En tal caso hacemos que el ID del logro tenga un primer caracter R que me ayuda a clasificarlo en otros metodos.
 			log="R"+log;
+			//Ahora se añade a la bd en Logros y llamamos a un metodo que lo mete en la bd como un logro de tipo LogroResolucion
 			ConexionBD.getConexionBD().actualizarBD("INSERT INTO LOGRO(ID_L,ID_SUDOKU,DESCRIPCION) VALUES('"+log+"','"+codS+"','"+descripcion+"');");
 			LogrosResolucion.logrosResolucion(log, numJug);
 		}
 	}
-	
+	//Este metodo se encarga de modificar los datos de un logro. AVISO: No se puede cambiar el tipo de logro. Si es un logro de resolucion no se modifica para que sea de puntuacionx
+	//En tal caso se debe eliminar y crear uno nuevo.
 	public static void modificarLogros(String codL,String codS,String descripcion,String puntos,String numJug) throws ExcepcionConectarBD{
+		//Primero modifica el logro en si(la descripcion y el sudoku asociado)
 		ConexionBD.getConexionBD().actualizarBD("UPDATE LOGRO SET ID_SUDOKU='"+codS+"',DESCRIPCION='"+descripcion+"' WHERE ID_L='"+codL+"';");
+		//Luego se encarga de mirar el primer caracter del id para saber que tipo de logro es y como hay que modificarlo.
 		if(codL.charAt(0)=='X'){
+			//Por ultimo se hacen llamadas a los metodos que modifican el tipo de logro.
 			LogrosPuntuacionX.modificarLogro(codL, puntos);
 		}
 		else if (codL.charAt(0)=='Y') {
