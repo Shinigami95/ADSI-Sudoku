b333b4d3d92221cea2fbb084eea73deca74841cd
Shinigami95 <Shinigami95@users.noreply.github.com>
2016-01-04 14:12:00 +0100

    Comentarios

--- Sudoku/src/packModelo/LogrosPuntuacionX.java
+++ Sudoku/src/packModelo/LogrosPuntuacionX.java
@@ -6,16 +6,11 @@ import packExcepciones.ExcepcionConectarBD;
 
 public class LogrosPuntuacionX extends Logros{
 	
-	String puntuacion;
-	
-	public LogrosPuntuacionX(String codL,String codS,String descripcion,String puntos){
-		super(codS,codL, descripcion);
-		this.puntuacion=puntos;
-		
-	}
+	//Metodo que mete el tipo de logro puntuacion x en la bd
 	public static void logrosPuntuacionX(String codL,String puntos) throws ExcepcionConectarBD{
 		ConexionBD.getConexionBD().actualizarBD("INSERT INTO LOGRO_PTOX(ID_L,PTO) VALUES('"+codL+"','"+puntos+"');");
 	}
+	//Metodo que modifica los datos  del logro de tipo logro puntuacion x en la bd
 	public static void modificarLogro(String codL,String puntos) throws ExcepcionConectarBD
 	{
 		ConexionBD.getConexionBD().actualizarBD("UPDATE LOGRO_PTOX SET PTO='"+puntos+"' WHERE ID_L='"+codL+"';");
